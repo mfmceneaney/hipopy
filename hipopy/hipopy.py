8a881642663c82451c09884bb1c291b04ee7dd32
Matthew McEneaney <matthew.mceneaney@duke.edu>
2022-08-23 16:20:47 -0400

    Removed DEBUGGING method.

--- hipopy/hipopy.py
+++ hipopy/hipopy.py
@@ -15,17 +15,6 @@ import hipopybind
 #----------------------------------------------------------------------#
 # Basic  I/O behaviors
 
-def get_random_ingredients(self,kind=None):
-    """
-    Return a list of random ingredients as strings.
-    :param kind: Optional "kind" of ingredients.
-    :type kind: list[str] or None
-    :raise lumache.InvalidKindError: If the kind is invalid.
-    :return: The ingredients list.
-    :rtype: list[str]
-    """
-    return ["shells", "gorgonzola", "parsley"]
-
 def open(filename,mode="r"):
     """
     Parameters
