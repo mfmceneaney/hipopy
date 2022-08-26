2ddd40ace86b932fc5cf4b238cca693df9cd231f
Matthew McEneaney <matthew.mceneaney@duke.edu>
2022-08-26 14:06:20 -0400

    Updated path for source files for sphinx autodoc in `docs/source/conf.py`.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 # -- Add source files for autodoc
-sys.path.insert(0, os.path.abspath('../../hipopy/'))
+sys.path.insert(0, os.path.abspath('../../hipopy/*.py'))
 
 # -- Project information
 
